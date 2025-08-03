2e2b3e6d41bcf297c919bee0a2750883978f3922
Ashish Joshi <ashish@Ashishs-MacBook-Air.local>
2025-08-03 21:51:46 +0530

    kadane

--- leetcode150+extras/array:string/Gas_Station.java
+++ leetcode150+extras/array:string/Gas_Station.java
@@ -1,5 +1,12 @@
 class Solution {
-    public int canCompleteCircuit(int[] gas, int[] cost) {
+
+    public static void main(String[] args) {
+        int[] gas = {1,2,3,4,5};
+        int[] cost = {3,4,5,1,2};
+        System.out.println(canCompleteCircuit(gas, cost));
+    }
+
+    public static int canCompleteCircuit(int[] gas, int[] cost) {
         int total_tank = 0, curr_tank = 0, start_index = 0;
         for (int i = 0; i < gas.length; i++) {
             total_tank += gas[i] - cost[i];
